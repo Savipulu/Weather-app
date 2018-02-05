79c6951cb1f3d24043a1fae99c02ce3b3c72fe0b
Savipulu <jouni.winter@gmail.com>
2018-02-05 03:18:08 +0200

    .

--- models/station.js
+++ models/station.js
@@ -1,5 +1,9 @@
 const mongoose = require("mongoose");
 
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
+
 const url = process.env.MONGODB_URI; //
 ("mongodb://Savipulu:TietoKantaSalaSana123@ds125318.mlab.com:25318/observations"); //process.env.MONGODB_URI;
 
