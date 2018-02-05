aa97d656c43ede991f7e523e8cb7ad1ffbeb8024
Savipulu <jouni.winter@gmail.com>
2018-02-05 02:49:21 +0200

    init

--- index.js
+++ index.js
@@ -63,7 +63,7 @@ app.put("/stations/:id", (req, res) => {
     });
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
